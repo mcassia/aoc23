7265a5495fb8531d9ba5647fa8c82dc834708d01
Matteo Cassia <matteo.cassia@me.com>
2023-12-14 07:25:15 +0000

    Clean up of solution to day 14

--- day14.py
+++ day14.py
@@ -1,6 +1,57 @@
 from utils import get_input_lines
 
 
+def solve_1():
+
+    layout = list(get_input_lines())
+
+    # Move up the boulders
+    for i, line in enumerate(layout):
+        if i == 0: continue
+        for j in range(i):
+            k = i - j
+            for l in range(len((line))):
+                if layout[k][l] == 'O' and layout[k-1][l] == '.':
+                    layout[k-1] = layout[k-1][:l] + 'O' + layout[k-1][l+1:]
+                    layout[k] = layout[k][:l] + '.' + layout[k][l+1:]
+
+    # Calculate the weight
+    return get_weight()
+
+
+def solve_2(n=1000000000):
+
+    # Parse the layout
+    layout = list(get_input_lines())
+
+    # To track transformation and determine the cycle
+    m = {}
+    weights = []
+    start, end = None, None
+
+    # Move the boulders
+    for i in range(n):
+        for j in range(4):
+            layout = apply(layout)
+            layout = rotate(layout)
+        weight = get_weight(layout)
+        weights.append(weight)
+        
+        # If the new, transformed layout was previously encountered, record the cycle details and
+        # stop transforming further
+        key = tuple(tuple(row) for row in layout)
+        if key in m:
+            start, end = m[key], i
+            break
+        else:
+            m[key] = i
+            
+    # Fast forward
+    weights = weights[start:end]
+    steps = n - start - 1
+    return weights[steps % len(weights)]
+
+
 def apply(layout):
     for i, line in enumerate(layout):
         if i == 0: continue
@@ -27,62 +78,3 @@ def get_weight(layout):
         row.count('O') * (len(layout) - i)
         for i, row in enumerate(layout)
     )
-
-
-def solve_2(n):
-
-    layout = list(get_input_lines())
-
-    m = {}
-    weights = []
-    start, end = None, None
-
-    # Move the boulders
-    for i in range(n):
-
-        for j in range(4):
-            layout = apply(layout)
-            layout = rotate(layout)
-        weight = get_weight(layout)
-        weights.append(weight)
-        
-        key = tuple(tuple(row) for row in layout)
-        if key in m:
-            start, end = m[key], i
-            break
-        else:
-            m[key] = i
-            
-    # Fast forward
-    weights = weights[start:end]
-    steps = n - start -1
-    return weights[steps % len(weights)]
-
-        
-
-
-
-
-
-
-def solve_1():
-
-    layout = list(get_input_lines())
-
-    # Move up the boulders
-    for i, line in enumerate(layout):
-        if i == 0: continue
-        for j in range(i):
-            k = i - j
-            for l in range(len((line))):
-                if layout[k][l] == 'O' and layout[k-1][l] == '.':
-                    layout[k-1] = layout[k-1][:l] + 'O' + layout[k-1][l+1:]
-                    layout[k] = layout[k][:l] + '.' + layout[k][l+1:]
-
-    # Calculate the weight
-    weight = sum(
-        row.count('O') * (len(layout) - i)
-        for i, row in enumerate(layout)
-    )
-
-    return weight
